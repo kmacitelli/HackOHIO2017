1d1b3d38a0376ea8580ce1ed0f6506cd790def58
Jared Mitten <mitten.13@osu.edu>
2017-10-21 10:58:15 -0400

    Initial project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HackOHIO2017.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
